13fd27ac22aa448145b3c0fab6beead39f5a2b57
crushr3sist <silent.death3500@gmail.com>
2024-05-13 02:57:36 +1000

    connection websocket

--- r3almX_backend/realtime_service/connection_service.py
+++ r3almX_backend/realtime_service/connection_service.py
@@ -1,9 +1,11 @@
 import asyncio
+import datetime
 import sys
 import traceback
 from queue import Queue
 from typing import Dict
 
+import redis
 from fastapi import Depends, WebSocket, WebSocketDisconnect
 from jose import JWTError, jwt
 
@@ -17,97 +19,99 @@ from r3almX_backend.realtime_service.main import realtime
 
 class Connection:
     def __init__(self):
-        # use redis to cache users connected
-
-        # connection cache status contains {user_id, status}
-        self.connection_status_cache: Dict[str, str] = self.get_status_cache()
-
-        # connection cache sockets contain {user_id, websocket}
+        self.redis_client = redis.Redis()
+        self.connection_status_cache: Dict[str, str] = {}
         self.connection_sockets: Dict[str, WebSocket] = {}
 
-    def connect(self):
-        # we add the user to the connection array
-        # but we implement a heartbeat so our server doesn't keep users in memory that aren't connected
-        ...
+    def connect(self, user_id):
+        self.connection_status_cache[user_id] = "online"
+        self.set_status_cache(user_id, "online")
 
-    def disconnect(self):
-        # remove the user from the heartbeat pool
-        ...
+    def disconnect(self, user_id):
+        if user_id in self.connection_status_cache:
+            del self.connection_status_cache[user_id]
+        if user_id in self.connection_sockets:
+            del self.connection_sockets[user_id]
+        self.set_status_cache(user_id, "offline")
 
-    def get_status_cache(self) -> Dict[str, str]:
-        # we get cache from redis and construct it into dict and return it
-        ...
+    def get_status_cache(self, user_id) -> Dict[str, str]:
+        cached_status = self.redis_client.hgetall("user_status")
+        return {
+            user_id.decode(): status.decode()
+            for user_id, status in cached_status.item()
+        }
 
-    def set_online(self):
-        # class A: we're gonna push users notifications
-        pass
-
-    def set_offline(self):
-        # class B: mobile notif push (web:toast, mobile: notif)
-        pass
-
-    def set_dnd(self):
+    def set_dnd(self, user_id):
         # class C: integer notif push (silent number increment)
         pass
 
     def is_connected(self, user_id) -> bool:
-        try:
-            if self.connection_status_cache.get(
-                f"{user_id}"
-            ) or self.connection_sockets.get(f"{user_id}"):
-                return True
-            return False
-        except KeyError as k:
-            return False
+        return (
+            user_id in self.connection_status_cache
+            or user_id in self.connection_sockets
+        )
 
     def get_status(self, user_id) -> str:
-        return self.connection_cache_status.get(f"{user_id}")
+        return self.connection_status_cache.get(user_id, "offline")
 
     def set_status(self, user_id, status):
-        # this is the function exposed to the websocket
-        # so we can update the status
-        # we also call set_status_cache
-        ...
+        if status in ["online", "offline", "dnd"]:
+            self.connection_status_cache[user_id] = status
+            self.set_status_cache(user_id, status)
+            getattr(self, f"set_{status}")(user_id)
 
     def set_status_cache(self, user_id, status):
-        # modifies the redis cache status of the user
-        ...
+        self.redis_client.hset("user_status", user_id, status)
 
 
 connection_manager = Connection()
 
 
-@realtime.websocket("/feedback")
-async def feedback(websocket: WebSocket, token: str, db=Depends(get_db)):
-    # simple websocket endpoint to receive feedback from the client so we know what users to keep in heartbeat pool
-    # automatic disconnect only, we have a connection websocket to connect to
-
-    ...
-
-
-@realtime.websocket("/connect")
+@realtime.websocket("/connection")
 async def connect(websocket: WebSocket, token: str, db=Depends(get_db)):
     # write a wholistic solution for connection so that we can send notif
-    user = get_user_from_token(token)
+    # we dont need a feedback websocket we need to just read the data from the client through this
+    user = get_user_from_token(token, db)
     if user:
         await websocket.accept()
-        while True:
-            # check the status of the user.
-            # if they're not in the connection sockets, then just connect them
-            # otherwise we need to check their last set status from the cache
-            # we need to also check the websocket request
-            # it should contain json with user id and requested change in connection
-            if connection_manager.is_connected(user.id) is False:
-                connection_manager.connection_socket[str(user.id)] = websocket
-                connection_manager.set_status_cache(
-                    connection_manager.get_status(user.id)
-                )
 
-            connection_change_request = await websocket.receive_json()
-            # through this we can just check for keys inside of the change request
-            if connection_change_request["status"]:
-                connection_manager.set_status(
-                    user.id, connection_change_request["status"]
-                )
+        connection_manager.connect(user.id)
+        connection_manager.connection_sockets[user.id] = websocket
+        last_activity = datetime.datetime.now()
+        heartbeat_interval = 30
+        expiry_timeout = 300
+        try:
+            while True:
+                try:
+                    if connection_manager.is_connected(user.id) is False:
+                        connection_manager.connection_socket[str(user.id)] = websocket
+                        connection_manager.set_status_cache(
+                            connection_manager.get_status(user.id)
+                        )
+                    connection_change_request = await websocket.receive_json()
+
+                    # through this we can just check for keys inside of the change request
+                    if connection_change_request["status"]:
+                        connection_manager.set_status(
+                            user.id, connection_change_request["status"]
+                        )
+                except asyncio.TimeoutError:
+                    try:
+                        await websocket.send_text("ping")
+                        await asyncio.wait_for(
+                            websocket.receive_text(), timeout=heartbeat_interval / 2
+                        )
+                        last_activity = datetime.now()
+
+                    except (asyncio.TimeoutError, WebSocketDisconnect):
+                        if (
+                            datetime.now() - last_activity
+                        ).total_seconds() > expiry_timeout:
+                            print(f"disconnecting user: {user.id} ")
+                            await websocket.close()
+                            connection_manager.disconnect(user.id)
+                            break
+        except WebSocketDisconnect:
+            connection_manager.disconnect(user.id)
     else:
         return websocket.close(1001)
