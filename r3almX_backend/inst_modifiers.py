32d197265e8246839a87733b67fa6391026331f0
crushr3sist <silent.death3500@gmail.com>
2024-12-30 03:56:13 +1000

    search init is automated and search works.

--- r3almX_backend/inst_modifiers.py
+++ r3almX_backend/inst_modifiers.py
@@ -12,15 +12,14 @@ async def init_database():
     except (
         sqlalchemy.exc.OperationalError,
         psycopg2.OperationalError,
-    ) as p:
-        logger.error(f"database connection error: {p}")
+    ):
         for _ in range(0, 5):
             try:
                 await init_db()
             except (
                 sqlalchemy.exc.OperationalError,
                 psycopg2.OperationalError,
-            ) as p:
+            ):
                 print("db connection hitch, retrying")
             else:
                 break
